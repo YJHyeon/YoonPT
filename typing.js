1219980178ced1503a9492941abb8108ec155836
Ubuntu <ubuntu@ip-172-31-44-127.ap-southeast-2.compute.internal>
2024-02-21 08:04:06 +0000

    Day4

---
+++ typing.js
@@ -0,0 +1,13 @@
+const content = "Hello, I'm JeongHyeon \n a front-end developer.";
+const text = document.querySelector(".text");
+let i = 0;
+
+function typing(){
+    let txt = content[i++];
+    text.innerHTML += txt=== "\n" ? "<br/>": txt;
+    if (i > content.length) {
+        text.textContent = "";
+        i = 0;
+    }
+}
+setInterval(typing, 120)
